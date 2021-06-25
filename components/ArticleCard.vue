b5e1fcd0480806b85d117dc29d284e5dd5017665
Sebastien-Bardin <33619819+Sebastien-Bardin@users.noreply.github.com>
2021-06-25 19:33:44 +0200

    front page restaurants

---
+++ components/ArticleCard.vue
@@ -0,0 +1,37 @@
+<template>
+<figure class="grid grid-cols-2  bg-gray-100 rounded-xl ">
+  <div class="pt-1 md:p-2 col-span-2 text-center md:text-left ">
+      <p class="text-lg font-semibold">
+        {{ ArticleName }}
+      </p>
+    <figcaption class="font-medium">
+      <div class="text-cyan-600">
+        {{ ArticleDescription }}
+      </div>
+      <div class="text-gray-500">
+        {{ ArticlePrice }}
+      </div>
+    </figcaption>
+  </div>
+  <img class="w-32 h-48 md:w-48 md:h-auto mt-1 rounded col-end-4 " src="https://www.zuerich.com/sites/default/files/web_zuerich_kindli_restaurant_1600x900_8375.jpg" alt="" width="384" height="512">
+</figure>
+
+</template>
+
+<script>
+export default {
+  name: 'ArticleCard',
+  props: {
+    ArticleName : String,
+    ArticleDescription : String,
+    ArticlePrice: String,
+
+  }
+}
+
+</script>
+
+
+<style>
+
+</style>
---
+++ pages/restorant.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id='resto'>
+    <img src="https://www.zuerich.com/sites/default/files/web_zuerich_kindli_restaurant_1600x900_8375.jpg"
+      class=" h-52 w-full object-cover ">
+
+    <div class=" w-full object-cover block">
+      <ul class="inline-flex">
+        <li class="m-3">
+          <a class="nav-link active" href="#">Menus</a>
+        </li>
+        <li class="m-3">
+          <a class="" href="#">Articles</a>
+        </li>
+        <li class="m-3">
+          <a class="" href="#">Autres</a>
+        </li>
+      </ul>
+    </div>
+    <div class="grid grid-cols-3 gap-4 m-4">
+      <ArticleCard v-for="article in articles" :key="article.name" ArticleName='Menu maxi BigMag' ArticleDescription="Meilleur Burger du MagDo" ArticlePrice="5" />
+    </div>
+  </div>
+
+</template>
+
+<script>
+import ArticleCard from '~/components/ArticleCard.vue'
+
+  export default {
+  name : 'resto',
+  components: { ArticleCard
+  },
+  data (){
+    return {
+      articles: [
+        {
+          description : 'blablalvlazlaldfz;fùazmer;fgazrga',
+          name : 'BigMag',
+          price : '5'
+        },
+        {
+          description : 'blablalvlazlaldfz;fùazmer;fgazrga',
+          name : 'BigMag',
+          price : '5'
+        },
+        {
+          description : 'blablalvlazlaldfz;fùazmer;fgazrga',
+          name : 'BigMag',
+          price : '5'
+        },
+        {
+          description : 'blablalvlazlaldfz;fùazmer;fgazrga',
+          name : 'BigMag',
+          price : '5'
+        },
+        {
+          description : 'blablalvlazlaldfz;fùazmer;fgazrga',
+          name : 'BigMag',
+          price : '5'
+        },
+        {
+          description : 'blablalvlazlaldfz;fùazmer;fgazrga',
+          name : 'BigMag',
+          price : '5'
+        }
+      ]
+    }
+
+  }
+}
+
+</script>
+
+
+<style>
+
+</style>
