311c4964e3934ad259c0925e873f9825b82c6a9a
Henri Briaux <henri.briaux@viacesi.fr>
2021-06-25 14:48:13 +0200

    nuxt

---
+++ layouts/default.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+      <div class="navbar"> 
+      Ceseat
+      <div class="navbar-onglet"> Commander </div>
+      <div class="navbar-onglet"> Mes commandes </div>
+      <div class="navbar-onglet"> Les restaurants </div>
+      </div>
+    <Nuxt />
+      <div class="footer"> 
+      <div class="footer-onglet"> A propos </div>
+      <div class="footer-onglet"> Nous contacter </div>
+      <div class="footer-onglet"> Support </div>
+      </div>
+
+  </div>
+</template>
+
+<style>
+  .navbar {
+    display:flex;
+    justify-content:space-around;
+    align-items:center;
+    font-size: 80px;
+    font-weight:bold;
+    font-family: 'Shrikhand', cursive;
+    color: #4686E7;    
+    margin:20px;
+    background-color:#FFFFFF;
+
+  }
+
+    .footer {
+    display:flex;
+    justify-content:flex-end;
+    align-items:center;
+    font-size: 80px;
+    font-weight:bold;
+    font-family: 'Shrikhand', cursive;
+    background-color: #4686E7;    
+    margin:20px;
+
+  }
+
+  .navbar-onglet {
+  font-size: 30px;
+  font-weight:bold;
+  font-family:'Quicksand';
+  color: #4686E7;    
+
+}
+  .footer-onglet {
+  font-size: 30px;
+  font-weight:bold;
+  font-family:'Quicksand';
+  color: #FFFFFF;    
+  margin:30px;
+
+}
+</style>
