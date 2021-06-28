420e15fcacc54a2e3142e3f3639399e6b262baf2
Sebastien-Bardin <33619819+Sebastien-Bardin@users.noreply.github.com>
2021-06-28 15:15:30 +0200

    création du store vuex

--- pages/restorant.vue
+++ pages/restorant.vue
@@ -1,73 +1,118 @@
 <template>
-  <div id='resto'>
+  <div id='resto' >
     <img src="https://www.zuerich.com/sites/default/files/web_zuerich_kindli_restaurant_1600x900_8375.jpg"
       class=" h-52 w-full object-cover ">
-
-    <div class=" w-full object-cover block">
+    <div ref="cat_bar" class=" w-full object-cover block bg-white" >
       <ul class="inline-flex">
         <li class="m-3">
-          <a class="nav-link active" href="#">Menus</a>
+          <a class="nav-link active" href="#menus">Menus</a>
         </li>
         <li class="m-3">
-          <a class="" href="#">Articles</a>
+          <a class="" href="#articles">Articles</a>
         </li>
         <li class="m-3">
           <a class="" href="#">Autres</a>
         </li>
       </ul>
     </div>
-    <div class="grid grid-cols-1 xl:grid-cols-3 gap-4 m-4">
-      <ArticleCard v-for="article in articles" :key="article.name" :ArticleName='article.name' :ArticleDescription="article.description" :ArticlePrice="article.price" />
+    <div>
+      <div id="menus">
+        <p class="text-lg font-medium ml-3 overflow-y-scroll">Menus</p>
+        <div class="grid grid-cols-1 xl:grid-cols-3 gap-4 m-4">
+          <ArticleCard v-for="article in articles" :key="article.name" :ArticleName='article.name'
+            :ArticleDescription="article.description" :ArticlePrice="article.price" />
+        </div>
+      </div>
+      <div id="articles">
+        <p class="text-lg font-medium ml-3">Articles</p>
+        <div class="grid grid-cols-1 xl:grid-cols-3 gap-4 m-4">
+          <ArticleCard v-for="article in articles" :key="article.name" :ArticleName='article.name'
+            :ArticleDescription="article.description" :ArticlePrice="article.price" />
+        </div>
+      </div>
     </div>
   </div>
 
 </template>
 
 <script>
-import ArticleCard from '~/components/ArticleCard.vue'
+  import ArticleCard from '~/components/ArticleCard.vue'
+
 
   export default {
-  name : 'resto',
-  components: { ArticleCard
-  },
-  data (){
-    return {
-      articles: [
-        {
-          description : 'blablalvlazlaldfz;fùazmer;',
-          name : 'BigMag',
-          price : '5'
-        },
-        {
-          description : 'blablalvlazlaldfz;fùazmer;fgazrga',
-          name : 'BigMag',
-          price : '5'
-        },
-        {
-          description : 'blablalvlazlaldfz;fùazmer;fgazrga',
-          name : 'BigMag',
-          price : '5'
-        },
-        {
-          description : 'blablalvlazlaldfz;fùazmer;fgazrga',
-          name : 'BigMag',
-          price : '5'
-        },
-        {
-          description : 'blablalvlazlaldfz;fùazmer;fgazrga',
-          name : 'BigMag',
-          price : '5'
-        },
-        {
-          description : 'blablalvlazlaldfz;fùazmer;fgazrga',
-          name : 'BigMag',
-          price : '5'
-        }
-      ]
-    }
+    name: 'resto',
+    components: {
+      ArticleCard
+    },
+    data() {
+      return {
+        articles: [{
+            description: 'blablalvlazlaldfz;fùazmer;',
+            name: 'BigMag',
+            price: '5'
+          },
+          {
+            description: 'blablalvlazlaldfz;fùazmer;fgazrga',
+            name: 'BigMag',
+            price: '5'
+          },
+          {
+            description: 'blablalvlazlaldfz;fùazmer;fgazrga',
+            name: 'BigMag',
+            price: '5'
+          },
+          {
+            description: 'blablalvlazlaldfz;fùazmer;fgazrga',
+            name: 'BigMag',
+            price: '5'
+          },
+          {
+            description: 'blablalvlazlaldfz;fùazmer;fgazrga',
+            name: 'BigMag',
+            price: '5'
+          },
+          {
+            description: 'blablalvlazlaldfz;fùazmer;fgazrga',
+            name: 'BigMag',
+            price: '5'
+          },
+          {
+            description: 'blablalvlazlaldfz;fùazmer;',
+            name: 'BigMag',
+            price: '5'
+          },
+          {
+            description: 'blablalvlazlaldfz;fùazmer;fgazrga',
+            name: 'BigMag',
+            price: '5'
+          },
+          {
+            description: 'blablalvlazlaldfz;fùazmer;fgazrga',
+            name: 'BigMag',
+            price: '5'
+          },
+          {
+            description: 'blablalvlazlaldfz;fùazmer;fgazrga',
+            name: 'BigMag',
+            price: '5'
+          },
+          {
+            description: 'blablalvlazlaldfz;fùazmer;fgazrga',
+            name: 'BigMag',
+            price: '5'
+          },
+          {
+            description: 'blablalvlazlaldfz;fùazmer;fgazrga',
+            name: 'BigMag',
+            price: '5'
+          }
+        ]
+      }
+    },
+    methods: {
 
+    },
   }
-}
 
 </script>
 
---
+++ store/index.js
@@ -0,0 +1,36 @@
+export const state = () => ({
+    commandes:[{
+        commande: {
+            restorant_name,
+            validated : false,
+            articles:[]
+        },
+    }],
+    user:{
+
+    }
+
+  })
+
+  export const mutations = {
+    addCommand(state,commande) {
+      state.commandes.push({
+            commande
+      })
+    },
+    removeCommand(state, commande) {
+      state.commandes.splice(state.commandes.indexOf(commande), 1)
+    },
+    addArticle(state, commande,article){
+        state.commandes.indexOf(commande).articles.push({article})
+    },
+    removeArticle(state, commande, article){
+        state.commandes.indexOf(commande).splice(state.commandes.indexOf(commande).indexOf(article),1)
+    }
+
+  }
+
+export const getters = {
+    commandes: state => state.commandes
+
+}
